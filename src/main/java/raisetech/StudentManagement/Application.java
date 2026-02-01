528da313f2766102732ddd753cfee98030ca9f0d
marukome <ogawahiroki@ogawadaikinoMacBook-Air.local>
2026-02-01 20:18:41 +0900

    24完了

--- src/main/java/raisetech/StudentManagement/Application.java
+++ src/main/java/raisetech/StudentManagement/Application.java
@@ -1,30 +1,14 @@
 package raisetech.StudentManagement;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-public class Application {
 
-	@Autowired
-	private StudentRepository repository;
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/studentList")
-	public List<Student> getStudentList() {
-		return repository.search();
-	}
-
-	@GetMapping("/studentCourseList")
-	public List<StudentCourse> getStudentCourseList() {
-		return repository.searchCourses();
-	}
 }
